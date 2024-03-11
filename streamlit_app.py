e2f706011016a6089aada675ecae76b9920c81a2
z0edby <162983396+z0edby@users.noreply.github.com>
2024-03-11 13:03:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,9 @@ name=st.text_input("ton nom")
 st.write("Hello"+name) 
 
 
-
-
 input= "lapin"
 list_possibilities=["rabbit","race","rhyme","rogue"]
 correct_answer="rabbit"
 st.right("traduis"+input)
-for i in range(len(list_possibilities))
+for i in range(len(list_possibilities)):
 st.button(list_possibilities[i])
