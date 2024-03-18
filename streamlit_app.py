1e852a8b7a81894403e164cca4daabe637a4d335
z0edby <162983396+z0edby@users.noreply.github.com>
2024-03-18 11:55:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st 
 import pandas as pd
 import numpy as hp
-voc=pd.nead_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vT-ZIygP_X0RCLp4r97Zub0bRwg1mtXtPABnlpFLFqsbnvb-qa_pEjrCFOGfd-xivTCc7QzWIQYL8iv/pub?output=csv')
+voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vT-ZIygP_X0RCLp4r97Zub0bRwg1mtXtPABnlpFLFqsbnvb-qa_pEjrCFOGfd-xivTCc7QzWIQYL8iv/pub?output=csv')
 st.dataframe(voc)
