4e08c875016072028bf25368f9972dd02c9ab22d
z0edby <162983396+z0edby@users.noreply.github.com>
2024-03-25 11:31:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,5 @@
 import streamlit as st 
 import pandas as pd
-import numpy as hp
-voc=pd.read_csv('https://docs.google.com/spreadsheets/d/e/2PACX-1vT-ZIygP_X0RCLp4r97Zub0bRwg1mtXtPABnlpFLFqsbnvb-qa_pEjrCFOGfd-xivTCc7QzWIQYL8iv/pub?output=csv')
-st.dataframe(voc)
-
-
-import streamlit as st 
-import pandas as pd
 import numpy as np
 voc = pd.read_csv( https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv ')
 l = voc.shape[0]
