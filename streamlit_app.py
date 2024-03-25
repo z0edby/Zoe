1deaf973200472ae906d9840da3de70e4b0bdd5d
z0edby <162983396+z0edby@users.noreply.github.com>
2024-03-25 12:02:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,10 @@ est-ce que la variable existe?
 if"my-var"in st.session_state
 supprimerla variable
 det st.session_state["my_var"]
+st.session_state["indices"]=indices
+si bravo:
+del st.session_state["indices"]
+a la place del a def de indices:
+if"indices"in st.session_state["indices"]
+else:
+  indices=np.random...
