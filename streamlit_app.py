2736357caf37279cb4793d204eb2051d401e8734
z0edby <162983396+z0edby@users.noreply.github.com>
2024-03-25 12:27:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
+import streamlit as st 
+import pandas as pd
+import numpy as np
+voc = pd.read_csv( "https://docs.google.com/spreadsheets/d/e/2PACX-1vSeARXO3MT92XWpg2IwyQOQ8Wi2upeEkqJvNJz5i3bRqHdJIrTchGBBclVu-3Jd1ohYKM4IxecgV64I/pub?output=csv ")
 l = voc.shape[0]
 if "indices" in st.session_state:
  indices=st.session_state["indices"]
@@ -9,16 +13,16 @@ st.write('Traduis: '+word_fr)
 
 def is_correct(i, j):
  if i==j:
-   st.write("Bien joué !")
-   if "indices" in st.session_state:
-     del st.session_state["indices"]
+  st.write("Bien joué !")
+  if "indices" in st.session_state:
+    del st.session_state["indices"]
  else:
    st.write("C'est perdu.")
    st.session_state["indices"]=indices
 col1, col2 = st.columns(2) 
 with col1:
-   for i in range(2):
-       st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
+ for i in range(2):
+   st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
 with col2:
-   for i in range(2,4):
-       st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
+ for i in range(2,4):
+   st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
