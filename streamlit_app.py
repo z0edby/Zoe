42e366a1a606e43de908bb7474f769848b98f6a7
z0edby <162983396+z0edby@users.noreply.github.com>
2024-03-25 12:06:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,24 @@
-- creation d'une variable:
-st.session_state["my_var"]=1
-- est-ce que la variable existe?
-if"my-var"in st.session_state
-- supprimerla variable
-det st.session_state["my_var"]
-st.session_state["indices"]=indices
-si bravo:
-del st.session_state["indices"]
-a la place del a def de indices:
-if"indices"in st.session_state["indices"]
+l = voc.shape[0]
+if "indices" in st.session_state:
+ indices=st.session_state["indices"]
 else:
-  indices=np.random...
+ indices = np.random.choice(l, size=4, replace=False)
+j = np.random.choice(indices)
+word_fr=voc['Définition'].values[j]
+st.write('Traduis: '+word_fr)
+
+def is_correct(i, j):
+ if i==j:
+   st.write("Bien joué !")
+   if "indices" in st.session_state:
+     del st.session_state["indices"]
+ else:
+   st.write("C'est perdu.")
+   st.session_state["indices"]=indices
+col1, col2 = st.columns(2) 
+with col1:
+   for i in range(2):
+       st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
+with col2:
+   for i in range(2,4):
+       st.button(voc["Hanzi"].values[indices[i]], on_click= is_correct, args=(indices[i],j))
